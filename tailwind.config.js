b271635845821f4e516e48d011e434a7c01ddd6c
a-garcia96 <alex.g9415@gmail.com>
2023-06-15 14:44:34 -0700

    flowbite setup for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // NEXTJS
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    //FLOWBITE
+    "./public/**/*.html",
+    "./node_modules/flowbite-react/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
