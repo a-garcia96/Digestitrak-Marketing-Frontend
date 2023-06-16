a9c93a2bd5f0e68b2d1e43770afb45f33230e561
Alex Garcia <>
2023-06-16 11:48:01 -0700

    fixed issue with tailwindcss not loading in dev env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // NEXTJS
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    //FLOWBITE
-    "./public/**/*.html",
-    "./node_modules/flowbite-react/**/*.js"
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin")
-  ],
+  plugins: [],
 }
+
