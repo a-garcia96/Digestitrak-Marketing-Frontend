eebad4931e2d14d1bff2616abf7443aeffade10d
a-garcia96 <alex.g9415@gmail.com>
2023-06-30 21:01:43 -0700

    styling

--- components/Logout/Logout.js
+++ components/Logout/Logout.js
@@ -22,7 +22,7 @@ const Logout = () => {
 
   return (
     <button
-      className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+      className="rounded-md bg-gray-200 px-5 py-2.5 text-sm font-medium text-teal-600"
       onClick={handleClick}
     >
       Logout
