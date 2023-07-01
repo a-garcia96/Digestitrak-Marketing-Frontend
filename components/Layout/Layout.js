4ca2d5eedc6747b2ab72e0301fb4fe912794a443
a-garcia96 <alex.g9415@gmail.com>
2023-06-30 20:49:33 -0700

    nav, footer and layout

---
+++ components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+import Footer from "../Footer/Footer";
+
+const Layout = ({children}) => {
+  return (
+    <>
+      <Navigation />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
