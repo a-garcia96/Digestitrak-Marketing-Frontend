6131765f738e1c1e06b243d719ccb0ebaf0899c1
a-garcia96 <alex.g9415@gmail.com>
2023-09-04 20:53:32 -0700

    fetch data for render on dashboard

--- components/Dashboard/Dashboard.js
+++ components/Dashboard/Dashboard.js
@@ -22,55 +22,75 @@ import {
   TableFooterCell,
   Badge,
 } from "@tremor/react";
+import ClipLoader from "react-spinners/ClipLoader";
+import { app } from "../../firebase/firebase";
+import {
+  collection,
+  where,
+  orderBy,
+  query,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
+import nookies from "nookies";
+import { useEffect, useState } from "react";
 
-export default function Example() {
-  const data = [
-    {
-      name: "Viola Amherd",
-      Role: "Federal Councillor",
-      departement:
-        "The Federal Department of Defence, Civil Protection and Sport (DDPS)",
-      status: "active",
-    },
-    {
-      name: "Simonetta Sommaruga",
-      Role: "Federal Councillor",
-      departement:
-        "The Federal Department of the Environment, Transport, Energy and Communications (DETEC)",
-      status: "active",
-    },
-    {
-      name: "Alain Berset",
-      Role: "Federal Councillor",
-      departement: "The Federal Department of Home Affairs (FDHA)",
-      status: "active",
-    },
-    {
-      name: "Ignazio Cassis",
-      Role: "Federal Councillor",
-      departement: "The Federal Department of Foreign Affairs (FDFA)",
-      status: "active",
-    },
-    {
-      name: "Ueli Maurer",
-      Role: "Federal Councillor",
-      departement: "The Federal Department of Finance (FDF)",
-      status: "active",
-    },
-    {
-      name: "Guy Parmelin",
-      Role: "Federal Councillor",
-      departement:
-        "The Federal Department of Economic Affairs, Education and Research (EAER)",
-      status: "active",
-    },
-    {
-      name: "Karin Keller-Sutter",
-      Role: "Federal Councillor",
-      departement: "The Federal Department of Justice and Police (FDJP)",
-      status: "active",
-    },
-  ];
+export default function Dashboard() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const db = getFirestore(app);
+      const cookies = nookies.get();
+
+      // make reference to collections
+      const symptomRef = collection(db, "symptom-tracker");
+      const mealRef = collection(db, "PHDiary");
+      //create query using reference and order by start time in descending order
+      const q1 = query(
+        mealRef,
+        where("uid", "==", cookies.uid),
+        orderBy("startTime", "desc")
+      );
+      const q2 = query(
+        symptomRef,
+        where("uid", "==", cookies.uid),
+        orderBy("startTime", "desc")
+      );
+      //get collection based on query
+      const querySnapshot1 = await getDocs(q1);
+      const querySnapshot2 = await getDocs(q2);
+
+      const mealData = querySnapshot1.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      const symptomData = querySnapshot2.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      setData({
+        mealData: [...mealData],
+        symptomData: [...symptomData],
+      });
+    }
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return (
+      <div className="absolute top-[50%] -translate-y-1/2 left-1/2 -translate-x-1/2">
+        <ClipLoader />
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-screen-xl mx-auto">
@@ -86,60 +106,98 @@ export default function Example() {
               </TabList>
               <TabPanels>
                 <TabPanel>
-                  <Title>List of Swiss Federal Councillours</Title>
                   <Table className="mt-5">
                     <TableHead>
                       <TableRow>
-                        <TableHeaderCell>Name</TableHeaderCell>
-                        <TableHeaderCell>Position</TableHeaderCell>
-                        <TableHeaderCell>Department</TableHeaderCell>
-                        <TableHeaderCell>Status</TableHeaderCell>
+                        <TableHeaderCell>Start Time</TableHeaderCell>
+                        <TableHeaderCell>End Time</TableHeaderCell>
+                        <TableHeaderCell>Full Meal</TableHeaderCell>
+                        <TableHeaderCell>Comments</TableHeaderCell>
                       </TableRow>
                     </TableHead>
                     <TableBody>
-                      {data.map((item) => (
-                        <TableRow key={item.name}>
-                          <TableCell>{item.name}</TableCell>
-                          <TableCell>
-                            <Text>{item.Role}</Text>
-                          </TableCell>
-                          <TableCell>
-                            <Text>{item.departement}</Text>
-                          </TableCell>
-                          <TableCell>
-                            <Badge color="emerald">{item.status}</Badge>
-                          </TableCell>
-                        </TableRow>
-                      ))}
+                      {data.mealData.map((item) => {
+                        let startDate = new Date(
+                          item.startTime.seconds * 1000
+                        ).toDateString();
+                        let endDate = new Date(
+                          item.endTime.seconds * 1000
+                        ).toDateString();
+                        let startTime = new Date(
+                          item.startTime.seconds * 1000
+                        ).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        });
+                        let endTime = new Date(
+                          item.endTime.seconds * 1000
+                        ).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        });
+
+                        return (
+                          <TableRow key={item.id}>
+                            <TableCell>{startDate + " " + startTime}</TableCell>
+                            <TableCell>
+                              <Text>{endDate + " " + endTime}</Text>
+                            </TableCell>
+                            <TableCell>
+                              <Text>
+                                {item.fullMeal == true ? "yes" : "no"}
+                              </Text>
+                            </TableCell>
+                            <TableCell>
+                              <Badge color="emerald">{item.comments}</Badge>
+                            </TableCell>
+                          </TableRow>
+                        );
+                      })}
                     </TableBody>
                   </Table>
                 </TabPanel>
                 <TabPanel>
-                  <Title>List of Swiss Federal Councillours</Title>
                   <Table className="mt-5">
                     <TableHead>
                       <TableRow>
-                        <TableHeaderCell>Name</TableHeaderCell>
-                        <TableHeaderCell>Position</TableHeaderCell>
-                        <TableHeaderCell>Department</TableHeaderCell>
-                        <TableHeaderCell>Status</TableHeaderCell>
+                        <TableHeaderCell>Start Time</TableHeaderCell>
+                        <TableHeaderCell>End Time</TableHeaderCell>
+                        <TableHeaderCell>Symptoms</TableHeaderCell>
                       </TableRow>
                     </TableHead>
                     <TableBody>
-                      {data.map((item) => (
-                        <TableRow key={item.name}>
-                          <TableCell>{item.name}</TableCell>
-                          <TableCell>
-                            <Text>{item.Role}</Text>
-                          </TableCell>
-                          <TableCell>
-                            <Text>{item.departement}</Text>
-                          </TableCell>
-                          <TableCell>
-                            <Badge color="emerald">{item.status}</Badge>
-                          </TableCell>
-                        </TableRow>
-                      ))}
+                      {data.symptomData.map((item) => {
+                        let startDate = new Date(
+                          item.startTime.seconds * 1000
+                        ).toDateString();
+                        let endDate = new Date(
+                          item.endTime.seconds * 1000
+                        ).toDateString();
+                        let startTime = new Date(
+                          item.startTime.seconds * 1000
+                        ).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        });
+                        let endTime = new Date(
+                          item.endTime.seconds * 1000
+                        ).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        });
+
+                        return (
+                          <TableRow key={item.id}>
+                            <TableCell>{startDate + " " + startTime}</TableCell>
+                            <TableCell>
+                              <Text>{endDate + " " + endTime}</Text>
+                            </TableCell>
+                            <TableCell>
+                              <Badge color="emerald">{item.comments}</Badge>
+                            </TableCell>
+                          </TableRow>
+                        );
+                      })}
                     </TableBody>
                   </Table>
                 </TabPanel>
@@ -153,11 +211,11 @@ export default function Example() {
           <div className="space-y-6">
             <Card decoration="top" decorationColor="teal">
               <Text>Meals this week</Text>
-              <Metric>21</Metric>
+              <Metric>{data.mealData.length}</Metric>
             </Card>
             <Card decoration="top" decorationColor="teal">
               <Text>Symptoms this week</Text>
-              <Metric>4</Metric>
+              <Metric>{data.symptomData.length}</Metric>
             </Card>
           </div>
         </Col>
