e8a5f2f937b100d3ec5efc14d5d4621f50df4bb9
a-garcia96 <alex.g9415@gmail.com>
2025-02-06 17:06:27 -0800

    setup for marketing frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,14 @@
 import { useEffect, useState } from "react";
 import "../styles/globals.css";
-import AuthContext from "../contexts/AuthContext";
-import { getAuthState } from "../firebase/firebaseAuth";
 import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState({});
 
-  useEffect(() => {
-    getAuthState(setUser);
-  }, []);
-
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthContext.Provider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
